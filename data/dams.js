151a4cb462c6ea6158a2b97da4bc2b72b8f83505
IRFAN NAZAR <97732665+irfannazar35@users.noreply.github.com>
2026-07-24 01:59:22 +0500

    Bundle dam features for reliable loading

---
+++ data/dams.js
@@ -0,0 +1,30 @@
+window.SD_DSS_DAMS_CSV = `Sr. No.,Name of Dam,District,Tehsil,Location,Type of Dam,Operational / Non-Operational,"Height
+(ft)","Completion Cost
+(million)","Gross Storage Capacity
+(Aft)","Live storage
+(Aft)","C.C.A.
+(Acres)","Capacity of Channel
+(Cfs)","Length of Canal
+(ft)","DSL
+(ft)","NPL
+(ft)","HFL
+(ft)","CWL
+(ft)",River / Nullah,Year of Completion,"Catchment Area
+(Sq. Km)",Decimal Latitude,Decimal Longitude
+1,Nirali ,Rawalpindi,Gujar Khan,It is at a distance of about & miles from Daultala Railway Station.,Earthfill,Operational,70,0.973,754,450,315,3,"12,672",1700.5,1715,1722,1715,Nirali Kas Tributary of Kanshi River,1970,9.19,33.117802,73.16613
+2,Dungi ,Rawalpindi,Gujar Khan,The dam site is located on Hachiara Nallah a tributory of River in Gujar Khan Tahsil of Rawalpindi District.,Concrete Gravity,Operational,71.4,1.47,"1,760",959,246,3,"16,282",1490,1502.5,1507.4,1502.5,Huchiara Kas Tributary of Kanshi River,1971,20,33.292583,73.30419
+3,Phalina ,Rawalpindi,Gujar Khan,"Dam Site is situated at 9 Km north east of Mandra Town, Tehsil Gujar Khan District Rawalpindi.",Earthfill,Operational,74,267.975,"3,900","2,930","2,200",21.6,"37,500",1630.6,1651.9,1656.6,1651.9,Phalina Kas Tributary of Kanshi River,2008,24.75,33.391845,73.311026
+4,Ugahan ,Rawalpindi,Gujar Khan,"Located at a distance of about 13.0 Km west of Gujar Khan town District Rawalpindi of Krumb Nullah, a tributary of Kanshi River.",Earthfill,Operational,85,509.602,"8,100","3,960","3,500",35,"39,500",1572.25,1594,1601,1594,Krumb Nullah Tributary of Kanshi River,2012,71.48,33.233114,73.239621
+5,Mohra Shera,Rawalpindi,Gujar Khan,"Near Mohra Shera village, District Rawalpindi",Earthfill,Operational,97,"1,128.54","11,800","5,820","4,500",30,"16,000",1536,1548,1556,1548,"Badala Kas, a tributary of Wadala Kas that outfalls in Soan",2025,127,33.338094,73.136669
+6,Garat  ,Jhelum,Dina,Dam site located west side Dina town Distt: Jhelum,Earthfill,Operational,66,14.31,"2,228","1,763",638,6,"16,526",922.5,951,955.5,951,Garat Kas Tributary of Kahan River,1982,14.01,33.018723,73.515906
+7,Jammargal  ,Jhelum,Jhelum,Dam site is near Jammergal village about 50 K.M south west of Distt: Jhelum.,Earthfill,Operational,53.48,22.98,"2,432","1,650",925,6.5,"15,840",879.25,890.81,897,890.81,Jammargal Kas Tributary of Jhelum River ,1992,15,32.746442,73.453019
+8,Tain Pura I ,Jhelum,Dina,Dam site is near ratwal railway station about 6 K.M. north west of Dina Town District Jhelum.,Earthfill,Operational,92.5,108.15,"5,941","5,775","3,410",30,"69,326",959,1000,1007,1000,Tainpura Kas Tributary of Kahan River,1994,35.5,33.057107,73.530376
+9,Tain Pura II,Jhelum,Dina,Dam site is near ratwal railway station about 6 K.M. north west of Dina Town District Jhelum.,Earthfill,Operational,85.75,0,"3,395","1,211",0,0,0,989,1018,1021,1018,Tainpura Kas Tributary of Kahan River,1994,9.2,33.068589,73.533078
+10,Lehri ,Jhelum,Dina,Site is located at Patha Wala Kas at a distance of 15 K.M.S in Tehsil noth east of Dina Town Tehsil: Dina Distt: Jhelum.,Earthfill,Operational,109,123.714,"5,705","4,595","2,220",30,"37,000",959,1000,1007,1000,Basli Nullah Tributary of Kahan River,2005,3.9,33.1094156,73.6069982
+11,Salial ,Jhelum,Sohawa,"Site is located at Local Kas, Tribotory of Basali Nullah Salial Kas about 40 K.M in siouth west of Dina Tehsil: Sohawa Distt: Jhelum.",Earthfill,Operational,68,43.4,527,417,372,4,"8,550",1118.75,1147,1153,1147,Potha Tributary of Jhelum River ,2005,16.5,32.878397,73.407941
+12,Pindori ,Jhelum,Dina,"Located at a distance of about 20.0 Km south west of mandra town, Tehsil Gujar khan District Rawalpindi on bodala kas a tributary of sowan River.",Concrete Gravity,Operational,147,954.874,"3,795","2,344","1,700",10,"17,300",984,1013.5,1026.63,1013.5,Potha Kas Tributary of Jhelum River ,2022,26.42,33.102584,73.604581
+13,Shah Habib,Jhelum,Jhelum,"Dam site is situated at 2 Km South west of Jammergal village, 55 Km from Jhelum City, Tehsil & District Jhelum.",Earthfill,Non-Operational,77.14,40.58,"1,655",536,450,3,"18,000",902,918.4,920.43,918.4,Local Nullah Jammargal Kas Tributary of Jhelum River ,2008,3.5,32.732888,73.461166
+14,Fatehpur ,Jhelum,Sohawa,"Dam site is situated at 34 Km south west of Domeli Town and 84 Km from JhelumCity, District Jhelum.",Earthfill,Operational,86.5,42.911,"1,733","1,534",315,3,"22,000",1537,1543,1547,1543,Nabnawala Kas Tributary of Bunha River,2008,3.5,32.9162521,73.1876546
+15,Domeli ,Jhelum,Sohawa,Site is located at Junc tion Point of Nili Pamal nullah near vellage Domeli Teh: Sohawa Distt: Jhelum.,Earthfill,Operational,120,320.537,"8,690","4,636","3,000",22,"28,000",1153,1175,1188,1175,Nuli and Pamal Nullah Tributary of Kahan River,2007,170,33.021721,73.323732
+16,Gurah Uttam ,Jhelum,Sohawa,"Dam site is situated at about 25 Kms South west from Domeli Town and 75 Km from Jhelum City, Tehsil & District Jhelum.",Earthfill,Operational,94,158.296,"2,679","1,030","1,500",12.25,"30,000",1373,1391,1400,1391,Sore Kas Tributary of Kahan River,2008,35.5,32.968326,73.237646
+`;
