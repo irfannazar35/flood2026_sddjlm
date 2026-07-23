238a17eeb49900a592ead6de7bb3034bb5c5aed6
IRFAN NAZAR <97732665+irfannazar35@users.noreply.github.com>
2026-07-24 01:24:02 +0500

    Add Google Sheets endpoint config

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.SD_DSS_CONFIG = {
+  // Paste your deployed Google Apps Script Web App URL here to make it the default for all users.
+  // Example: https://script.google.com/macros/s/AKfycb.../exec
+  sheetsWebAppUrl: ''
+};
